0ee8fbde21763125eba4268ed8f6be2dc352237f
h15945 <953071808@qq.com>
2018-11-14 21:31:39 +0800

    Update index.php

--- bin/server/files/index.php
+++ bin/server/files/index.php
@@ -1,22 +1,6 @@
 <?php
 
-
-/*
-if(!$_COOKIE['think_language']){
-	header("Location: http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'?l=zh-CN');
-}
-*/
-//var_dump();
-//exit();
-define('THINK_PATH', './ThinkPHP/');
-define('APP_PATH', './App/');
-define('APP_NAME', 'App');
-define('APP_DEBUG', true);  
-define('BUILD_DIR_SECURE', true);
-define('DIR_SECURE_FILENAME', 'default.html');
-define('DIR_SECURE_CONTENT', '  ');
-function readover(){return true;}//定义一个函数，用于防止跨脚本访问
-require (THINK_PATH.'ThinkPHP.php');
+echo "this is a php script";
 
 
 ?>
